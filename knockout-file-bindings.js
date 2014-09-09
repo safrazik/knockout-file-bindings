c4f8234ef6dc9a4096da2bc3e6d7b7cc24868fe1
mrsafraz <mr.safraz@gmail.com>
2014-09-10 01:25:54 +0530

    Fix dependancy for jquery

--- knockout-file-bindings.js
+++ knockout-file-bindings.js
@@ -13,13 +13,13 @@
 
     if (typeof require === "function" && typeof exports === "object" && typeof module === "object") {
         // CommonJS or Node: hard-coded dependency on "knockout"
-        factory(require("knockout"));
+        factory(require("knockout"), require("jquery"));
     } else if (typeof define === "function" && define["amd"]) {
         // AMD anonymous module with hard-coded dependency on "knockout"
         define(["knockout", "jquery"], factory);
     } else {
         // <script> tag: use the global `ko` object, attaching a `mapping` property
-        factory(ko);
+        factory(ko, jQuery);
     }
 }(function (ko, $) {
 
